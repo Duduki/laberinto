d41f3480358e82beb0ec4676211149cff3669430
Duduki <ducaslon@gmail.com>
2019-05-13 23:02:25 +0200

    Se agrego el ejercicio sorteo

---
+++ dia7aleatorio.py
@@ -0,0 +1,9 @@
+#creamos un archivo nuevo
+#guardamos con la carpeta del repositorio
+#con la extension.py
+#uso de numeros aleatorios
+
+#importamos la libreria randint  
+from random import randint 
+aleatorio=randint(0,20)   #creamos una variable y generamos un numero aleatorio entre 0 y 20
+print(aleatorio)  #imprimimos el numero generdo
